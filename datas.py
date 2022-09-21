39c04a90eb9bc7b9bdcac17bc38bfe70b7b0b088
mathpereira1 <88118960+mathpereira1@users.noreply.github.com>
2022-09-21 16:26:36 -0300

    Initial commit

---
+++ datas.py
@@ -0,0 +1,14 @@
+class Data():
+    def __init__(self, dia, mes, ano):
+        self.data_formatada = f'{dia}/{mes}/{ano}'
+
+    def formatada(self):
+        print(self.data_formatada)
+
+
+# Métodos, que definem o comportamento de uma classe
+# Criação de métodos
+# Como chamar métodos através do objeto
+# Acesso aos atributos através do self
+# Garbage Collector
+# O tipo None
